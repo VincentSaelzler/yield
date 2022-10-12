2ec13a33a58c081409a9660879553bd4a7a1c031
Vincent Saelzler <vincent.saelzler@gmail.com>
2022-10-11 22:13:50 -0400

    for in loop

--- Program.fs
+++ Program.fs
@@ -1,2 +1,9 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System;
+
+let randomNumbers = [2; 3; 7; 8]
+
+printfn "%A" randomNumbers
+
+for n in randomNumbers do
+    Console.Write("About to Check ")
+    Console.WriteLine(n)
